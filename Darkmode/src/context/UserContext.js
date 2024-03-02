04c4881d30a34614166044de7dcb78474b077fcc
harshchoudhary981066 <harsh981066@gmail.com>
2024-03-02 14:06:53 +0530

    Context and Context Provider pushed

---
+++ Darkmode/src/context/UserContext.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+//This is the first step in creating a user context. In the next step we need to create a provider too
+//This file is '.js' because it is purely a javascript file
